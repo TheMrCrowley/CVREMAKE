07646de397f201ab683c511f400c51a28f2f0834
Denis Mythnik <themrcrovvley@gmail.com>
2021-09-17 16:59:51 +0300

    feat: implement overflow at home

--- js/script.js
+++ js/script.js
@@ -2,11 +2,34 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     const menuBtn = document.querySelector('.burger__btn');
-    menuBtn.addEventListener('click', (e) => {
-        console.log(e.target);
+    const menu = document.querySelector('.header-menu');
+
+    menuBtn.addEventListener('click', togleClasses);
+
+    function togleClasses() {
         menuBtn.classList.toggle('burger__btn_active');
-        setTimeout(() => {
-            menuBtn.classList.toggle('burger__btn_rotate');
-        }, 500);
-    });
+        menu.classList.toggle('header-menu_active');
+        menuBtn.classList.toggle('burger__btn_rotate');
+        // if (menu.classList.contains('header-menu_active')) {
+        //     window.addEventListener('click', togleClasses);
+        // } else {
+        //     window.removeEventListener('click', togleClasses);
+        // }
+    }
+
+    function clickOverflow(e) {
+        const target = e.target;
+        if (isOverflow(target)) {
+            console.log('here');
+        }
+    }
+
+    function isOverflow(element) {
+        if (!(element.classList.contains('burger__btn') && element.classList.contains('burger__line') &&
+        element.classList.contains('header-menu__list'))) {
+            return false;
+        }
+        return true;
+    }
+    window.addEventListener('click', clickOverflow);
 });
